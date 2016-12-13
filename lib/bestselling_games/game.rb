3fe1ae0303f899d0c63b49ee40ff3050797db4b4
John Schlesinger <jschles1@ramapo.edu>
2016-12-13 04:50:41 +0000

    Added part of xbox_scrape that scrapes game data and outputs each game's title

--- lib/bestselling_games/game.rb
+++ lib/bestselling_games/game.rb
@@ -2,7 +2,13 @@ class BestsellingGames::Game
   attr_accessor :title, :price, :publisher, :rating, :url
 
   def self.xbox_scrape
-
+    return_array = []
+    doc = Nokogiri::HTML(open("http://www.gamestop.com/browse/games/xbox-one?nav=28-xu0,131e0-ffff2418"))
+    games = doc.css("div.product.new_product")
+    games.each_with_index do |game, i|
+      game = self.new
+      game.title = games.css(".ats-product-title-lnk")[i].text.gsub("- Only at GameStop", "").strip
+    end
   end
 
   def self.ps4_scrape
