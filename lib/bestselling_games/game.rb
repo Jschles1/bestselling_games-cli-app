c24c8146b9045bc40bb7d4a44a39689c722df294
John Schlesinger <jschles1@ramapo.edu>
2016-12-13 05:09:55 +0000

    Copied, pasted, and adjusted code for ds_scrape

--- lib/bestselling_games/game.rb
+++ lib/bestselling_games/game.rb
@@ -95,7 +95,34 @@ class BestsellingGames::Game
   end
 
   def self.ds_scrape
+    return_array = []
+    doc = Nokogiri::HTML(open("http://www.gamestop.com/browse/games/nintendo-3ds?nav=131a2-ffff2418"))
+    games = doc.css("div.product.new_product")
+    games.each_with_index do |game, i|
+      game = self.new
+      game.title = games.css(".ats-product-title-lnk")[i].text.gsub("- Only at GameStop", "").strip
 
+      price_scrape = games.css("p.pricing.ats-product-price")[i].text.strip
+      if price_scrape.length > 7
+        game.price = price_scrape.gsub("$", " ").split(" ")[1].insert(0, "$")
+      else
+        game.price = price_scrape
+      end
+
+      game.publisher = games.css(".publisher.ats-product-publisher")[i].text.gsub("by ", "").strip
+
+      if games.css("p.rating.ats-product-esrbRating strong em")[i] != nil
+        game.rating = games.css("p.rating.ats-product-esrbRating strong em")[i].text.strip
+      else
+        game.rating = "No Rating"
+      end
+
+      game.url = games.css(".ats-product-title-lnk")[i].attr("href").split.insert(0, "www.gamestop.com").join
+      return_array << game
+    end
+
+    return_array
+    
   end
 
   def self.pc_scrape
