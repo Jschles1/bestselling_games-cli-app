3d9d97cd66a684cd444b3bce8e683543b397d5b0
John Schlesinger <jschles1@ramapo.edu>
2016-12-13 04:48:04 +0000

    Completed welcome, put all methods in call

--- lib/bestselling_games/cli.rb
+++ lib/bestselling_games/cli.rb
@@ -1,11 +1,16 @@
 class BestsellingGames::CLI
 
 def call
-
+  welcome
+  list_bestsellers
+  get_details
+  decision
 end
 
 def welcome
-
+  puts "Welcome to the Best Selling Games App!"
+  sleep 1
+  print "Which system's current bestselling games would you like to see? (Xbox One, PS4, Wii U, 3DS, PC): "
 end
 
 def list_bestsellers
