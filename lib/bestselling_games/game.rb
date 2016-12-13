a9f24a9daa18ff57f0310d24f6765535277216d4
John Schlesinger <jschles1@ramapo.edu>
2016-12-13 05:04:41 +0000

    Took code from xbox_scrape and adjusted it for ps4_scrape

--- lib/bestselling_games/game.rb
+++ lib/bestselling_games/game.rb
@@ -33,6 +33,33 @@ class BestsellingGames::Game
   end
 
   def self.ps4_scrape
+    return_array = []
+    doc = Nokogiri::HTML(open("http://www.gamestop.com/browse/games/playstation-4?nav=28-xu0,131dc-ffff2418"))
+    games = doc.css("div.product.new_product")
+    games.each_with_index do |game, i|
+      game = self.new
+      game.title = games.css(".ats-product-title-lnk")[i].text.gsub("- Only at GameStop", "").strip
+
+      price_scrape = games.css("p.pricing.ats-product-price")[i].text.strip
+      if price_scrape.length > 7
+        game.price = price_scrape.gsub("$", " ").split(" ")[1].insert(0, "$")
+      else
+        game.price = price_scrape
+      end
+
+      game.publisher = games.css(".publisher.ats-product-publisher")[i].text.gsub("by ", "").strip
+
+      if games.css("p.rating.ats-product-esrbRating strong em")[i] != nil
+        game.rating = games.css("p.rating.ats-product-esrbRating strong em")[i].text.strip
+      else
+        game.rating = "No Rating"
+      end
+
+      game.url = games.css(".ats-product-title-lnk")[i].attr("href").split.insert(0, "www.gamestop.com").join
+      return_array << game
+    end
+
+    return_array
 
   end
 
