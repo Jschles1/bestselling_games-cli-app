450ce0a88af9462ff467578c1494837d0289d49e
John Schlesinger <jschles1@ramapo.edu>
2016-12-20 23:51:29 +0000

    Removed ps4_scrape

--- lib/bestselling_games/game.rb
+++ lib/bestselling_games/game.rb
@@ -15,30 +15,6 @@ class BestsellingGames::Game
     @@all
   end
 
-  def self.ps4_scrape
-    return_array = []
-    doc = Nokogiri::HTML(open("http://www.gamestop.com/browse/games/playstation-4?nav=28-xu0,131dc-ffff2418"))
-    games = doc.css("div.product.new_product")
-    games.each_with_index do |game, i|
-      game = self.new
-      game.title = games.css(".ats-product-title-lnk")[i].text.gsub("- Only at GameStop", "").strip
-
-      price_scrape = games.css("p.pricing.ats-product-price")[i].text.strip
-      if price_scrape.length > 7
-        game.price = price_scrape.gsub("$", " ").split(" ")[1].insert(0, "$")
-      else
-        game.price = price_scrape
-      end
-
-      game.publisher = games.css(".publisher.ats-product-publisher")[i].text.gsub("by ", "").strip
-      game.url = games.css(".ats-product-title-lnk")[i].attr("href").split.insert(0, "www.gamestop.com").join
-      return_array << game
-    end
-
-    return_array
-
-  end
-
   def self.wiiu_scrape
     return_array = []
     doc = Nokogiri::HTML(open("http://www.gamestop.com/browse/games/nintendo-wii-u?nav=131b0-ffff2418"))
