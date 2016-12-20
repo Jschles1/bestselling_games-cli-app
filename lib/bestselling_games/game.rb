53c93054d7cdf7c50ca91431c93384a66a7622eb
John Schlesinger <jschles1@ramapo.edu>
2016-12-20 23:42:15 +0000

    Moved xbox_scrape to scraper, defined initialize and all methods

--- lib/bestselling_games/game.rb
+++ lib/bestselling_games/game.rb
@@ -1,28 +1,18 @@
 class BestsellingGames::Game
   attr_accessor :title, :price, :publisher, :url
 
-  def self.xbox_scrape
-    return_array = []
-    doc = Nokogiri::HTML(open("http://www.gamestop.com/browse/games/xbox-one?nav=28-xu0,131e0-ffff2418"))
-    games = doc.css("div.product.new_product")
-    games.each_with_index do |game, i|
-      game = self.new
-      game.title = games.css(".ats-product-title-lnk")[i].text.gsub("- Only at GameStop", "").strip
+  @@all = []
 
-      price_scrape = games.css("p.pricing.ats-product-price")[i].text.strip
-      if price_scrape.length > 7
-        game.price = price_scrape.gsub("$", " ").split(" ")[1].insert(0, "$")
-      else
-        game.price = price_scrape
-      end
+  def initialize(title, price, publisher, url)
+    self.title = title
+    self.price = price
+    self.publisher = publisher
+    self.url = url
+    @@all << self
+  end
 
-      game.publisher = games.css(".publisher.ats-product-publisher")[i].text.gsub("by ", "").strip
-      game.url = games.css(".ats-product-title-lnk")[i].attr("href").split.insert(0, "www.gamestop.com").join
-      return_array << game
-    end
-
-    return_array
-
+  def self.all
+    @@all
   end
 
   def self.ps4_scrape
