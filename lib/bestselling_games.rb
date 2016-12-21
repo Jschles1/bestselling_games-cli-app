ead81ca28fe6fd9d4bfad1da0489cd061a347eb5
John Schlesinger <jschles1@ramapo.edu>
2016-12-21 00:21:48 +0000

    Added require for scraper.rb

--- lib/bestselling_games.rb
+++ lib/bestselling_games.rb
@@ -5,6 +5,7 @@ require 'pry'
 require "bestselling_games/version"
 require "bestselling_games/game"
 require "bestselling_games/cli"
+require "bestselling_games/scraper"
 
 module BestsellingGames
   # Your code goes here...
