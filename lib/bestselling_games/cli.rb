c439de1cd8b43f11d6408e55ccbc25a0aca649de
Jschles1 <jschles1@ramapo.edu>
2017-12-02 16:19:33 -0500

    Added comments

--- lib/bestselling_games/cli.rb
+++ lib/bestselling_games/cli.rb
@@ -6,22 +6,28 @@ def call
 end
 
 def welcome
+  # Welcomes user and gives options on what system's bestsellers they would like to see.
   puts "Welcome to the Best Selling Games App!"
   sleep 1
   print "Which system's current bestselling games would you like to see? (Xbox One, PS4, Switch, Wii U, 3DS, Switch, PC): "
 end
 
 def list_bestsellers
+  # Gets user input.
   input = nil
   input = gets.strip.downcase
 
+  # Takes user input to decide which system's bestsellers to retrieve.
   case input
   when "xbox one"
+    # Delete any game instances from previous queries.
     BestsellingGames::Game.all.clear
+    # Call scrape class method with url of requested system.
     BestsellingGames::Scraper.scrape("https://www.gamestop.com/browse/games/xbox-one?nav=28-xu0,131e0-ffff2418")
     @bestsellers = BestsellingGames::Game.all
     puts "Current Xbox One Bestsellers:"
 
+    # Prints each bestseller with their associated number on the list.
     @bestsellers.each.with_index(1) do |game, i|
       puts "#{i}. #{game.title}"
     end
@@ -90,11 +96,13 @@ def list_bestsellers
 end
 
 def get_details
+  # Gets user input again.
   input = nil
   puts ""
   print "Please enter the number of the game you want more details on: "
   input = gets.strip.downcase
 
+  # Lists have between 1-12 items, so only those inputs will be valid.
   if input.to_i > 0 && input.to_i < 13
     game = @bestsellers[input.to_i - 1]
     puts "*******************************************************************"
@@ -113,6 +121,7 @@ def get_details
 end
 
 def decision
+  # Takes user input to determine what the user wants to do next.
   puts "Type (1) if you would like to get details on another game from the list."
   puts "Type (2) if you would like to choose another system's bestsellers to list."
   puts "Type (3) if you would like to exit the program."
--- lib/bestselling_games/scraper.rb
+++ lib/bestselling_games/scraper.rb
@@ -2,6 +2,7 @@ class BestsellingGames::Scraper
   def self.scrape(url)
     doc = Nokogiri::HTML(open(url))
     games = doc.css("div.product.new_product")
+    # Scrapes text from elements and uses it to create Game objects with associated attributes.
     games.each_with_index do |game, i|
       title = games.css(".ats-product-title-lnk")[i].text.gsub("- Only at GameStop", "").strip
       price_scrape = games.css("p.pricing.ats-product-price")[i].text.strip
